3b0904cdee34d739dd8c8b5e5d90b7fbe3914764
Dimores <xxkirito31@gmail.com>
2023-09-08 18:23:11 -0300

    Adicionando classes de excecao de: Cliente, Peca, Relatorio de Manutencao e de Rede, Servico

---
+++ GerenciadordeHardware/src/main/java/model/exceptions/PecaException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.exceptions;
+
+/**
+ *
+ * @author diego
+ */
+public class PecaException extends RuntimeException{
+
+    public PecaException(String message) {
+        super(message);
+    }
+    
+}
